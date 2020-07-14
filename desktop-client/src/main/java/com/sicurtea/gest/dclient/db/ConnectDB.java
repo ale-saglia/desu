f0be85f4f52a2f9af70481c2d7647d9e29e66c7f
Alessandro Saglia <ale.saglia@gmail.com>
2020-07-14 11:18:28 +0200

    Setting up ConnectDB

--- desktop-client/src/main/java/com/sicurtea/gest/dclient/db/ConnectDB.java
+++ desktop-client/src/main/java/com/sicurtea/gest/dclient/db/ConnectDB.java
@@ -7,13 +7,45 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 
 public class ConnectDB {
-    //mvn dependency:sourcesprivate static final String jdbcURL = 
+    static String CONFIG_FILE_NAME = "dbSetting.yaml";
 
     public static void main(String args[]) {
-        Connection c = null;
+        Connection conn = null;
+        Session session = null;
+
+        String dbHost = null;
+        int dbPort = 0;
+        String dbUser = null;
+        String dbPassword = null;
+        String dbName = null;
+
+        String sshUser = null;
+        String sshHost = null;
+        int sshPort = 0;
+        String sshPassword = null;
+
+        //TODO reading file variables from YAML config
+
         try {
+            // SSH connection setup && port forwarding
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no"); // Set StrictHostKeyChecking property to no to avoid
+                                                       // UnknownHostKey issue
+            JSch jsch = new JSch();
+            session = jsch.getSession(sshUser, sshHost, sshPort);
+            session.setPassword(sshPassword);
+            session.setConfig(config);
+            session.connect();
+            System.out.println("Connected");
+            int assinged_port = session.setPortForwardingL(dbPort, dbHost, dbPort);
+            System.out.println("localhost:" + assinged_port + " -> " + dbPort + ":" + dbPort);
+            System.out.println("Port Forwarded");
+
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb", "postgres", "123");
+
+            conn = DriverManager.getConnection(
+                    "jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName, dbUser,
+                    dbPassword);
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
--- desktop-client/src/main/java/module-info.java
+++ desktop-client/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.sicurtea.gest.dclient {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires jsch;
 
     opens com.sicurtea.gest.dclient to javafx.fxml;
     exports com.sicurtea.gest.dclient;

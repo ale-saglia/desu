e44f0980e5d380c72351b0cd3cf081224447a54e
Alessandro Saglia <ale.saglia@gmail.com>
2020-07-21 09:30:35 +0200

    Formatting code

--- desktop-client/src/main/java/model/Account.java
+++ desktop-client/src/main/java/model/Account.java
@@ -3,15 +3,15 @@ package model;
 import java.util.Set;
 
 public class Account {
-    String fiscalCode;
-    String name;
-    String numberVAT;
-    String atecoCode;
-    String legalAddress;
-    String category;
-    Set<Contact> contacts;
-    
-    public Account(String fiscalCode, String name, String numberVAT, String atecoCode, String legalAddress,
+	String fiscalCode;
+	String name;
+	String numberVAT;
+	String atecoCode;
+	String legalAddress;
+	String category;
+	Set<Contact> contacts;
+
+	public Account(String fiscalCode, String name, String numberVAT, String atecoCode, String legalAddress,
 			String category) {
 		this.fiscalCode = fiscalCode;
 		this.name = name;
@@ -21,48 +21,33 @@ public class Account {
 		this.category = category;
 	}
 
-
-
 	public String getFiscalCode() {
 		return fiscalCode;
 	}
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public String getNumberVAT() {
 		return numberVAT;
 	}
-
-
 
 	public String getAtecoCode() {
 		return atecoCode;
 	}
 
-
-
 	public String getLegalAddress() {
 		return legalAddress;
 	}
-
-
 
 	public String getCategory() {
 		return category;
 	}
 
+	private class Contact {
+		// TODO enum description types
+		String contact;
 
-
-	private class Contact{
-        //TODO enum description types
-        String contact;
-
-
-    }
+	}
 }

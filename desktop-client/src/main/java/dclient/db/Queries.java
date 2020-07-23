d7b78d5e80d6338f3c638add4a94beffdf19ec96
Alessandro Saglia <ale.saglia@gmail.com>
2020-07-23 09:03:09 +0200

    Modify query to also fetch notes

--- desktop-client/src/main/java/dclient/db/Queries.java
+++ desktop-client/src/main/java/dclient/db/Queries.java
@@ -4,33 +4,36 @@ public class Queries {
 
     public static String getRSPPtable() {
         return (
-            "   SELECT *  "  + 
-            "   FROM   (SELECT r.rspp_jobid        AS jobid,  "  + 
-            "                  r.jobstart          AS jobstart,  "  + 
-            "                  r.jobstart          AS jobend,  "  + 
-            "                  a.\"name\"          AS \"name\",  "  + 
-            "                  a.customer_category AS category,  "  + 
-            "                  r.invoiceid         AS invoiceid,  "  + 
-            "                  i.payed             AS payed  "  + 
-            "           FROM   accounts.accounts a,  "  + 
-            "                  deadlines.rspp r,  "  + 
-            "                  invoices.invoices i,  "  + 
-            "                  jobs.jobs j  "  + 
-            "           WHERE  r.invoiceid = i.invoiceid  "  + 
-            "           UNION  "  + 
-            "           SELECT r.rspp_jobid        AS jobid,  "  + 
-            "                  r.jobstart          AS jobstart,  "  + 
-            "                  r.jobstart          AS jobend,  "  + 
-            "                  a.\"name\"          AS \"name\",  "  + 
-            "                  a.customer_category AS category,  "  + 
-            "                  NULL                AS invoiceid,  "  + 
-            "                  false               AS payed  "  + 
-            "           FROM   accounts.accounts a,  "  + 
-            "                  deadlines.rspp r,  "  + 
-            "                  jobs.jobs j  "  + 
-            "           WHERE  r.invoiceid IS NULL) AS tab  "  + 
-            "   ORDER  BY \"name\",  "  + 
-            "            jobend    " 
+            "SELECT *\r\n" + 
+            "FROM   (SELECT r.rspp_jobid AS jobid,\r\n" + 
+            "               r.jobstart   AS jobstart,\r\n" + 
+            "               r.jobstart   AS jobend,\r\n" + 
+            "               a.fiscalcode AS fiscalcode,\r\n" + 
+            "               a.customer_category  as category,\r\n" + 
+            "               a.\"name\"     AS \"name\",\r\n" + 
+            "               r.invoiceid  AS invoiceid,\r\n" + 
+            "               i.payed      AS payed\r\n" + 
+            "        FROM   accounts.accounts a,\r\n" + 
+            "               deadlines.rspp r,\r\n" + 
+            "               invoices.invoices i,\r\n" + 
+            "               jobs.jobs j\r\n" + 
+            "        WHERE  r.invoiceid = i.invoiceid\r\n" + 
+            "        UNION\r\n" + 
+            "        SELECT r.rspp_jobid AS jobid,\r\n" + 
+            "               r.jobstart   AS jobstart,\r\n" + 
+            "               r.jobstart   AS jobend,\r\n" + 
+            "               a.fiscalcode AS fiscalcode,\r\n" + 
+            "               a.customer_category  as category,\r\n" + 
+            "               a.\"name\"     AS \"name\",\r\n" + 
+            "               NULL         AS invoiceid,\r\n" + 
+            "               false        AS payed\r\n" + 
+            "        FROM   accounts.accounts a,\r\n" + 
+            "               deadlines.rspp r,\r\n" + 
+            "               jobs.jobs j\r\n" + 
+            "        WHERE  r.invoiceid IS NULL) AS tab\r\n" + 
+            "       FULL OUTER JOIN deadlines.rspp_notes rn\r\n" + 
+            "                    ON tab.fiscalcode = rn.fiscalcode\r\n" + 
+            "ORDER  BY \"name\", jobend  " 
         );
     }
 }

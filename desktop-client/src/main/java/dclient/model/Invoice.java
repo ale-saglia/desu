ea975cd482d46a95fed68fcebda50e5b9c6caf34
Alessandro Saglia <ale.saglia@gmail.com>
2020-07-23 10:32:56 +0200

    Add invoice class

---
+++ desktop-client/src/main/java/dclient/model/Invoice.java
@@ -0,0 +1,31 @@
+package dclient.model;
+
+import java.time.LocalDate;
+
+public class Invoice {
+	
+	String id;
+	int number;
+	LocalDate emission;
+	String type;
+	Boolean payed;
+	
+	public Invoice(String id, int number, LocalDate emission, String type, Boolean payed) {
+		this.id = id;
+		this.number = number;
+		this.emission = emission;
+		this.type = type;
+		this.payed = payed;
+	}
+
+	public Invoice(int number, LocalDate emission, String type, Boolean payed) {
+		this.number = number;
+		this.emission = emission;
+		this.type = type;
+		this.payed = payed;
+		createID();
+	}
+	public void createID() {
+		this.id = (number + "/" + type + " " + emission);
+	}
+}

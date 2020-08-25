59076694ffb5f85cb38e972d337aae714aafe7ec
Alessandro Saglia <ale.saglia@gmail.com>
2020-08-25 11:47:32 +0200

    Removed unused import

--- desktop-client/src/main/java/dclient/db/SicurteaDAO.java
+++ desktop-client/src/main/java/dclient/db/SicurteaDAO.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;

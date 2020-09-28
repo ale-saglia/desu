035e147dd9697e117f4728c90d10cc4f6c0e8316
Alessandro Saglia <ale.saglia@gmail.com>
2020-09-28 15:16:41 +0200

    Add feature to be implemented

---
+++ desktop-client/src/main/java/dclient/controllers/ControllerCommon.java
@@ -0,0 +1,10 @@
+package dclient.controllers;
+
+import javafx.scene.control.DatePicker;
+
+public class ControllerCommon {
+    //TODO Implements this https://stackoverflow.com/questions/32346893/javafx-datepicker-not-updating-value
+    public static LocalDate getDate(DatePicker datePicker){
+
+    }
+}
